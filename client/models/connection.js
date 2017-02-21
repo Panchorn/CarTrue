d1f088cf2b90628b4d30147987f54292a7c738ee
nnpanc <panchorn.lert@g.swu.ac.th>
2017-02-21 21:52:02 +0700

    update_21022017

--- client/models/connection.js
+++ client/models/connection.js
@@ -1,30 +1,16 @@
 //serv true
 const mongoose = require('mongoose');
-var opt = {
-        user: 'cartrueSWUrw',
-        pass: '8kiNmi^dy[,L;',
-        auth: {
-            authdb: 'PLEASE_READ'
-        }
-    };
-var db = mongoose.createConnection( '61.90.233.80', 'PLEASE_READ', 27017, opt);
+// var opt = {
+//         user: 'cartrueSWUrw',
+//         pass: '8kiNmi^dy[,L;',
+//         auth: {
+//             authdb: 'PLEASE_READ'
+//         }
+//     };
+var db = mongoose.createConnection( '61.90.233.80', 'PLEASE_READ', 27017);
 
 module.exports.Connection = db;
 module.exports.Mongoose = mongoose;
-
-// //serv lnw
-// const mongoose = require('mongoose');
-// var opt = {
-//         user: 'admin02',
-//         pass: 'pass',
-//         auth: {
-//             authdb: 'CarTrue'
-//         }
-//     };
-// var db = mongoose.createConnection( '47.88.241.73', 'CarTrue', 27017, opt);
-
-// module.exports.Connection = db;
-// module.exports.Mongoose = mongoose;
 
 
 // //serv localhost
